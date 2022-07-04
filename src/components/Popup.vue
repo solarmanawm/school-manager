97516178ae03235e195309672ba920609f4f399e
Dmitry Fisunov <solarman.awm@gmail.com>
2022-07-04 14:47:13 +0300

    Create the Popup component

---
+++ src/components/Popup.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+            v-if="visible"
+            class="inset-0 fixed z-10 flex items-center justify-center"
+    >
+        <div
+                @click="close"
+                class="inset-0 absolute bg-gray-800 opacity-90 z-0"
+        ></div>
+        <div class="w-full relative z-10 xs:max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
+            <app-card class="bg-white">
+                <slot></slot>
+            </app-card>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps, defineEmits} from "vue";
+
+import AppCard from './Card.vue'
+
+interface Props {
+    visible: boolean;
+}
+
+interface Emits {
+    (e: 'update:visible', value: boolean): void;
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const close = () => {
+    emit('update:visible', false)
+}
+
+const name = 'Popup'
+</script>
--- src/components/pages/Students.vue
+++ src/components/pages/Students.vue
@@ -1,18 +1,31 @@
 <template>
-    <app-context-buttons>
+    <Teleport to="#app-context-buttons">
         <app-button
                 @click="addStudent"
         >New Student</app-button>
-    </app-context-buttons>
+    </Teleport>
+
+    <Teleport to="#app-popup">
+        <app-popup
+                v-model:visible="visible"
+        >
+            <p>popup</p>
+        </app-popup>
+    </Teleport>
 </template>
 
 <script setup lang="ts">
-import AppContextButtons from '../shared/ContextButtons.vue'
+// @ts-ignore
+import {ref} from "vue";
+
+import AppPopup from '../Popup.vue'
 // @ts-ignore
 import AppButton from '../Button.vue'
 
 const addStudent = () => {
+    visible.value = true
 }
 
+const visible = ref(false)
 const name = 'Students'
 </script>
