4545a28a31743c7f56b6a36e279a22d128b4493c
Dmitry Fisunov <solarman.awm@gmail.com>
2022-07-12 13:15:00 +0300

    Use icon in the ButtonSet component

--- src/components/ButtonSet.vue
+++ src/components/ButtonSet.vue
@@ -5,7 +5,13 @@
                 :variant="option.variant"
                 class="flex-1 rounded-none first:rounded-bl-md first:rounded-tl-md last:rounded-br-md last:rounded-tr-md"
                 @click="change(option.value)"
-        >{{ option.title }}
+        >
+            <template v-if="option.icon">
+                <i :class="option.icon"></i>
+            </template>
+            <template v-else>
+                {{ option.title }}
+            </template>
         </app-button>
     </div>
 </template>
@@ -16,6 +22,7 @@ import {computed, defineProps, defineEmits} from 'vue'
 import AppButton, {Variant} from './Button.vue'
 
 export interface Option {
+    icon?: string;
     title: string;
     value: string;
 }
@@ -25,6 +32,7 @@ interface Emits {
 }
 
 interface Props {
+    variant?: string;
     modelValue: string;
     options: Option[];
 }
@@ -34,7 +42,7 @@ const props = defineProps<Props>()
 const opts = computed(() => {
     return props.options.map((option) => ({
         ...option,
-        variant: props.modelValue === option.value ? Variant.PRIMARY : Variant.SECONDARY,
+        variant: props.modelValue === option.value ? Variant.PRIMARY : props.variant || Variant.SECONDARY,
     }))
 })
 const change = (value: string) => {
