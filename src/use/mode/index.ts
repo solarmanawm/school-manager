fc93fd8603b97f76b3fb2d04f98a3cc880743aa0
Dmitry Fisunov <solarman.awm@gmail.com>
2022-07-12 10:21:22 +0300

    Create the useMode hook

---
+++ src/use/mode/index.ts
@@ -0,0 +1,34 @@
+// @ts-ignore
+import {computed, ComputedRef, ref} from 'vue'
+
+interface UseMode<T> {
+    is: (modeToCompare: ValueOf<T>) => boolean;
+    set: (newMode: ValueOf<T>) => void;
+    current: ComputedRef<string>;
+}
+
+type ValueOf<T> = T[keyof T];
+
+export function useMode<T>(modes: T): UseMode<T> {
+    const values = Object.values(modes)
+    const mode = ref('')
+    const shadowMode = computed(() => mode.value)
+
+    const is = (modeToCompare: ValueOf<T>) => {
+        return mode.value === modeToCompare
+    }
+
+    const set = (newMode: ValueOf<T>) => {
+        if (!values.includes(newMode)) {
+            return
+        }
+
+        mode.value = newMode
+    }
+
+    return {
+        is,
+        set,
+        current: shadowMode,
+    }
+}
