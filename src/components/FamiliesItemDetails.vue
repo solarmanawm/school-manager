7a859e3edfe1cdf734378075dba142a44a182819
Dmitry Fisunov <solarman.awm@gmail.com>
2022-07-22 09:21:35 +0300

    Prepare family item details for pie chart

--- src/components/FamiliesItemDetails.vue
+++ src/components/FamiliesItemDetails.vue
@@ -24,7 +24,7 @@
     <div class="flex items-center justify-between">
         <div class="flex flex-col item-start">
             <p class="text-gray-300 text-sm mb-2 mb-6">ID: {{ item.id }}</p>
-            <p class="mb-6 text-blue-500 font-bold text-4xl">Income: {{ item.income }} <i class="fa-solid fa-ruble-sign"></i></p>
+            <p class="mb-6 text-blue-500 font-bold text-3xl">Income: {{ item.income }} <i class="fa-solid fa-ruble-sign"></i></p>
         </div>
 
         <div class="flex">
@@ -70,8 +70,31 @@
                     class="flex"
             >
                 <app-card class="flex-1">
-                    <p class="font-bold text-sm mb-3">Fees:</p>
-                    <p>{{ fees }}</p>
+                    <app-grid-row>
+                        <app-grid-col
+                                :class="{
+                                    'w-1/2': hasDescription,
+                                    'w-2/3': !hasDescription,
+                                }"
+                        >
+                            <p class="font-bold text-sm mb-3">Fees:</p>
+                            <div
+                                    v-for="fee of fees"
+                                    :key="fee.id"
+                                    class="mt-3 first:mt-0 bg-gray-100 rounded-md px-4 py-2 border border-gray-200"
+                            >
+                                <p class="text-sm font-bold capitalize mb-1">{{ fee.name }}</p>
+                                <p class="font-bold text-blue-500">{{ fee.value }} <i class="fa-solid fa-ruble-sign"></i></p>
+                            </div>
+                        </app-grid-col>
+                        <app-grid-col
+                                :class="{
+                                    'w-1/2': hasDescription,
+                                    'w-1/3': !hasDescription,
+                                }"
+                        >
+                        </app-grid-col>
+                    </app-grid-row>
                 </app-card>
             </app-grid-col>
         </app-grid-row>
@@ -106,7 +129,21 @@ const dataStore = useDataStore()
 const item = computed(() => dataStore.getFamilyById(route.params.id))
 const itemExists = computed(() => !!item.value)
 const title = computed(() => itemExists.value ? item.value.name : '')
-const fees = computed(() => itemExists.value ? item.value.fees : [])
+const fees = computed(() => itemExists.value
+        ? item.value.fees.map((feeId: string) => {
+            const fee = dataStore.getFeeById(feeId)
+            return {
+                id: fee.id,
+                name: fee.name,
+                value: fee.value,
+            }
+        })
+        : []
+)
+const totalFees = computed(() => fees.value.reduce((acc: number, current: {value: number}) => {
+    acc += +current.value
+    return acc
+}, 0))
 const hasDescription = computed(() => !!item.value.description)
 const hasFees = computed(() => fees.value.length > 0)
 const updateTitle = (title: string) => {
